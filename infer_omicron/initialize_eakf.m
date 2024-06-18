e6ac9cc41e56b0e782f7d6230a06daa5047e3d35
Roselyn F Kaondera-Shava <112559656+roseshava@users.noreply.github.com>
2024-06-18 14:09:10 -0400

    Add files via upload

---
+++ infer_omicron/initialize_eakf.m
@@ -0,0 +1,72 @@
+
+function x=initialize_eakf(Pop,num_ens)
+
+
+num_loc=size(Pop,1);
+N=Pop;
+
+Sl=0.15; Su=0.8; %lower, upper fraction
+El=0; Eu=0.0001;
+Irl=0; Iru=0;
+Iul=0; Iuu=0;
+
+% % % %estimate all parameters
+betal=0.5;betau=2;%transmission rate 
+mul=0.8;muu=0.8;%relative transmissibility
+Zl=4; Zu=4;
+alphal=0.004;alphau=0.1;
+Dl=4; Du=4; %infectious period D=3/2
+
+%%range of model state including variables and parameters
+xmin=[];
+xmax=[];
+for i=1:num_loc
+    xmin=[xmin,Sl*N(i),El*N(i),Irl*N(i),Iul*N(i),0];
+    xmax=[xmax,Su*N(i),Eu*N(i),Iru*N(i),Iuu*N(i),0];
+
+end
+
+xmin=[xmin,mul,Zl,Dl,ones(1,num_loc)*betal,ones(1,num_loc)*alphal];
+xmax=[xmax,muu,Zu,Du,ones(1,num_loc)*betau,ones(1,num_loc)*alphau];
+
+%%%lower bounds \alpha
+
+xmin(70)=0.007;
+xmin(71)=0.02;
+xmin(72)=0.03;
+xmin(73)=0.01;
+xmin(74)=0.14;
+xmin(75)=0.03;
+xmin(76)=0.004;
+xmin(77)=0.01;
+xmin(78)=0.01;
+xmin(79)=0.009;
+xmin(80)=0.005;
+
+
+xmax(74)=0.2;%maputo cidade
+
+
+
+%%Latin Hypercubic Sampling
+x=lhsu(xmin,xmax,num_ens);
+x=x';
+
+for i=1:num_loc
+    x((i-1)*5+1:(i-1)*5+4,:)=round(x((i-1)*5+1:(i-1)*5+4,:));
+end
+
+
+
+
+%% LHS from uniform distribution
+function s=lhsu(xmin,xmax,nsample)
+
+nvar=length(xmin);
+ran=rand(nsample,nvar);
+s=zeros(nsample,nvar);
+for j=1: nvar
+    idx=randperm(nsample);
+   P =(idx'-ran(:,j))/nsample;
+   s(:,j) = xmin(j) + P.* (xmax(j)-xmin(j));
+end
---
+++ infer_omicron/model_eakf.m
@@ -0,0 +1,230 @@
+function [x,obs]=model_eakf(x,ts,Pop)
+
+dt=1; %integrate forward for one day
+tmstep=1;%data is daily
+[~,num_ens]=size(x);
+num_loc=size(Pop,1);
+N=Pop; %population
+
+%S,E,Is,Ia,obs,...,beta,mu,theta,Z,alpha,D
+Sidx=(1:5:5*num_loc)';
+Eidx=(2:5:5*num_loc)';
+Iridx=(3:5:5*num_loc)';
+Iuidx=(4:5:5*num_loc)';
+obsidx=(5:5:5*num_loc)';
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+muidx=num_loc*5+1; %mu: adjustable parameter
+Zidx=num_loc*5+2; %alpha: adjustable parameter
+Didx=num_loc*5+3;  %D: duration of infection
+%betaidx=num_loc*5+4; % beta: transmission rate
+%alphaidx=num_loc*5+5; %Z: average latency period
+
+betaidx= (num_loc*5+4:num_loc*5+14)';
+alphaidx= (num_loc*5+15:num_loc*5+25)';
+
+
+
+%%transform format from state space vector to matrix
+S=zeros(num_loc,num_ens,abs(tmstep)+1);
+E=zeros(num_loc,num_ens,abs(tmstep)+1);
+Ir=zeros(num_loc,num_ens,abs(tmstep)+1);
+Iu=zeros(num_loc,num_ens,abs(tmstep)+1);
+
+Incidence=zeros(num_loc,num_ens,abs(tmstep)+1);
+obs=zeros(num_loc,num_ens);
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%% 
+
+%initialize S,E,Ir,Iu and parameters
+S(:,:,1)=x(Sidx,:);
+E(:,:,1)=x(Eidx,:);
+Ir(:,:,1)=x(Iridx,:);
+Iu(:,:,1)=x(Iuidx,:);
+
+mu=x(muidx,:);
+Z=x(Zidx,:);
+D=x(Didx,:);
+beta=x(betaidx,:);
+alpha=x(alphaidx,:);
+
+
+%%%%%%%%%%%%%%%%%%%%%
+%start integration
+
+tcnt=0;
+
+for t=ts+dt:dt:ts+tmstep
+    tcnt=tcnt+1; %Run model forward
+
+
+    %first step
+   
+    dt1=dt;
+
+    Eexpr=dt1*(ones(num_loc,1).*beta).*S(:,:,tcnt).*Ir(:,:,tcnt)./N;
+    Eexpu=dt1*(ones(num_loc,1)*mu).*(ones(num_loc,1).*beta).*S(:,:,tcnt).*Iu(:,:,tcnt)./N;
+    Einfr=dt1*(ones(num_loc,1).*alpha).*E(:,:,tcnt)./(ones(num_loc,1)*Z);
+    Einfu=dt1*(ones(num_loc,1).*(1-alpha)).*E(:,:,tcnt)./(ones(num_loc,1)*Z);
+    Erecr=dt1*Ir(:,:,tcnt)./(ones(num_loc,1)*D);
+    Erecu=dt1*Iu(:,:,tcnt)./(ones(num_loc,1)*D);
+    
+   
+    Eexpr=max(Eexpr,0);Eexpu=max(Eexpu,0);
+    Einfr=max(Einfr,0);Einfu=max(Einfu,0);
+    Erecr=max(Erecr,0);Erecu=max(Erecu,0);
+
+    %%%%%%STOCHASTIC version
+    Eexpr=poissrnd(Eexpr);
+    Eexpu=poissrnd(Eexpu);
+    Einfr=poissrnd(Einfr);
+    Einfu=poissrnd(Einfu);
+    Erecr=poissrnd(Erecr);
+    Erecu=poissrnd(Erecu);
+
+        
+    sk1=-Eexpr-Eexpu;
+    ek1=Eexpr+Eexpu-Einfr-Einfu;
+    irk1=Einfr-Erecr;
+    iuk1=Einfu-Erecu;
+    ik1i=Einfr;
+
+          
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%second step
+
+
+    Ts1=S(:,:,tcnt)+sk1/2;
+    Te1=E(:,:,tcnt)+ek1/2;
+    Tir1=Ir(:,:,tcnt)+irk1/2;
+    Tiu1=Iu(:,:,tcnt)+iuk1/2;
+
+
+    Eexpr=dt1*(ones(num_loc,1).*beta).*Ts1.*Tir1./N;
+    Eexpu=dt1*(ones(num_loc,1)*mu).*(ones(num_loc,1).*beta).*Ts1.*Tiu1./N;
+    Einfr=dt1*(ones(num_loc,1).*alpha).*Te1./(ones(num_loc,1)*Z);
+    Einfu=dt1*(ones(num_loc,1).*(1-alpha)).*Te1./(ones(num_loc,1)*Z);
+    Erecr=dt1*Tir1./(ones(num_loc,1)*D);
+    Erecu=dt1*Tiu1./(ones(num_loc,1)*D);
+
+    
+   
+    Eexpr=max(Eexpr,0);Eexpu=max(Eexpu,0);
+    Einfr=max(Einfr,0);Einfu=max(Einfu,0);
+    Erecr=max(Erecr,0);Erecu=max(Erecu,0);
+
+     %%%%%%STOCHASTIC version
+    Eexpr=poissrnd(Eexpr);
+    Eexpu=poissrnd(Eexpu);
+    Einfr=poissrnd(Einfr);
+    Einfu=poissrnd(Einfu);
+    Erecr=poissrnd(Erecr);
+    Erecu=poissrnd(Erecu);
+   
+    sk2=-Eexpr-Eexpu;
+    ek2=Eexpr+Eexpu-Einfr-Einfu;
+    irk2=Einfr-Erecr;
+    iuk2=Einfu-Erecu;
+    ik2i=Einfr;
+
+   
+
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %third step
+    Ts2=S(:,:,tcnt)+sk2/2;
+    Te2=E(:,:,tcnt)+ek2/2;
+    Tir2=Ir(:,:,tcnt)+irk2/2;
+    Tiu2=Iu(:,:,tcnt)+iuk2/2;
+
+
+    Eexpr=dt1*(ones(num_loc,1).*beta).*Ts2.*Tir2./N;
+    Eexpu=dt1*(ones(num_loc,1)*mu).*(ones(num_loc,1).*beta).*Ts2.*Tiu2./N;
+    Einfr=dt1*(ones(num_loc,1).*alpha).*Te2./(ones(num_loc,1)*Z);
+    Einfu=dt1*(ones(num_loc,1).*(1-alpha)).*Te2./(ones(num_loc,1)*Z);
+    Erecr=dt1*Tir2./(ones(num_loc,1)*D);
+    Erecu=dt1*Tiu2./(ones(num_loc,1)*D);
+
+   
+    Eexpr=max(Eexpr,0);Eexpu=max(Eexpu,0);
+    Einfr=max(Einfr,0);Einfu=max(Einfu,0);
+    Erecr=max(Erecr,0);Erecu=max(Erecu,0);
+
+     %%%%%%STOCHASTIC version
+    Eexpr=poissrnd(Eexpr);
+    Eexpu=poissrnd(Eexpu);
+    Einfr=poissrnd(Einfr);
+    Einfu=poissrnd(Einfu);
+    Erecr=poissrnd(Erecr);
+    Erecu=poissrnd(Erecu);
+    
+   
+    sk3=-Eexpr-Eexpu;
+    ek3=Eexpr+Eexpu-Einfr-Einfu;
+    irk3=Einfr-Erecr;
+    iuk3=Einfu-Erecu;
+    ik3i=Einfr;
+
+   
+    %fourth step
+ 
+    Ts3=S(:,:,tcnt)+sk3;
+    Te3=E(:,:,tcnt)+ek3;
+    Tir3=Ir(:,:,tcnt)+irk3;
+    Tiu3=Iu(:,:,tcnt)+iuk3; 
+    
+    Eexpr=dt1*(ones(num_loc,1).*beta).*Ts3.*Tir3./N;
+    Eexpu=dt1*(ones(num_loc,1)*mu).*(ones(num_loc,1).*beta).*Ts3.*Tiu3./N;
+    Einfr=dt1*(ones(num_loc,1).*alpha).*Te3./(ones(num_loc,1)*Z);
+    Einfu=dt1*(ones(num_loc,1).*(1-alpha)).*Te3./(ones(num_loc,1)*Z);
+    Erecr=dt1*Tir3./(ones(num_loc,1)*D);
+    Erecu=dt1*Tiu3./(ones(num_loc,1)*D);
+
+    
+
+    Eexpr=max(Eexpr,0);Eexpu=max(Eexpu,0);
+    Einfr=max(Einfr,0);Einfu=max(Einfu,0);
+    Erecr=max(Erecr,0);Erecu=max(Erecu,0);
+
+    %%%%%%STOCHASTIC version
+    Eexpr=poissrnd(Eexpr);
+    Eexpu=poissrnd(Eexpu);
+    Einfr=poissrnd(Einfr);
+    Einfu=poissrnd(Einfu);
+    Erecr=poissrnd(Erecr);
+    Erecu=poissrnd(Erecu);
+    
+    sk4=-Eexpr-Eexpu;
+    ek4=Eexpr+Eexpu-Einfr-Einfu;
+    irk4=Einfr-Erecr;
+    iuk4=Einfu-Erecu;
+    ik4i=Einfr;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    S(:,:,tcnt+1)=S(:,:,tcnt)+round(sk1/6+sk2/3+sk3/3+sk4/6);
+    E(:,:,tcnt+1)=E(:,:,tcnt)+round(ek1/6+ek2/3+ek3/3+ek4/6);
+    Ir(:,:,tcnt+1)=Ir(:,:,tcnt)+round(irk1/6+irk2/3+irk3/3+irk4/6);
+    Iu(:,:,tcnt+1)=Iu(:,:,tcnt)+round(iuk1/6+iuk2/3+iuk3/3+iuk4/6);
+    Incidence(:,:,tcnt+1)=round(ik1i/6+ik2i/3+ik3i/3+ik4i/6);
+    obs=Incidence(:,:,tcnt+1);
+
+    
+end
+
+
+%%%%%%%%%%%%%%%%%
+%update x
+
+%%%update x
+x(Sidx,:)=S(:,:,tcnt+1);
+x(Eidx,:)=E(:,:,tcnt+1);
+x(Iridx,:)=Ir(:,:,tcnt+1);
+x(Iuidx,:)=Iu(:,:,tcnt+1);
+x(obsidx,:)=obs;
+
+
+
+
+
